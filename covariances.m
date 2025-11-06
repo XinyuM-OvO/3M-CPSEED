d43fa11af2ac1b2dcb036e79f24f0eeee65c865b
XinyuM-OvO <xinyuu_ovo@163.com>
2025-11-06 23:32:01 +0800

    Update covariances.m
    change comment

--- covariances.m
+++ covariances.m
@@ -1,14 +1,15 @@
 function cov_matrix = covariances(data)
-% 输入: data 为三维矩阵 [channels × samples × trials]
-% 输出: cov_matrix 为协方差矩阵 [channels × channels × trials]
+% input: "data" is a three-dimensional matrix [channels × samples × trials]
+% output: cov_matrix is the covariance matrix [channels × channels × trials]
 
 [channels, samples, trials] = size(data);
 cov_matrix = zeros(channels, channels, trials);
 
 for i = 1:trials
-    trial_data = data(:, :, i);  % 提取第i个试次的数据
-    trial_data = trial_data - mean(trial_data, 2);  % 去除均值（中心化）
-    cov_trial = (trial_data * trial_data') / (samples - 1);  % 计算协方差
+    trial_data = data(:, :, i);  % Extract the data of the i-th trial
+    trial_data = trial_data - mean(trial_data, 2);  % Remove the mean (centralization)
+    cov_trial = (trial_data * trial_data') / (samples - 1);  % Calculate the covariance
     cov_matrix(:, :, i) = cov_trial;
 end
+
 end
